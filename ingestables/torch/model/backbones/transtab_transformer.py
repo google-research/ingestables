de47dd2d8e48f45f067a24b9b73ddc7c68303d47
Hana Joo <hanajoo@google.com>
2026-07-29 19:50:13 -0700

    Add pyrefly suppressions
    PiperOrigin-RevId: 956226366

--- ingestables/torch/model/backbones/transtab_transformer.py
+++ ingestables/torch/model/backbones/transtab_transformer.py
@@ -175,7 +175,7 @@ class TransTabEncoder(nn.Module):
           activation=activation,
       )
       stacked_transformer = nn.TransformerEncoder(
-          encoder_layer, num_layers=num_layer - 1
+          encoder_layer, num_layers=num_layer - 1  # pyrefly: ignore[bad-argument-type]
       )
       self.transformer_encoder.append(stacked_transformer)
 
